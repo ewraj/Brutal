704eb0a9774f313999d15484da69df6a99a0723e
aashishaxo <singhaashish4522@gmail.com>
2026-03-23 23:32:00 +0530

    some changes

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ function renderSidebar() {
         
         const editBtn = document.createElement('button');
         editBtn.className = 'edit-chat-btn';
-        editBtn.innerHTML = '&#9998;';
+        editBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg>';
         editBtn.title = 'Edit Chat';
         editBtn.onclick = (e) => {
             e.stopPropagation();
